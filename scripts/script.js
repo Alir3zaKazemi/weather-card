72093a5591196958d0c80da2f5fc91cbcd43141b
Alir3za <alireza2212kazemi@gmail.com>
2024-12-17 14:24:15 +0330

    fix some issues

--- scripts/script.js
+++ scripts/script.js
@@ -3,18 +3,26 @@
 let city = document.getElementById("city-name");
 let temp = document.getElementById("tempreture-num");
 let weatherCondition = document.getElementById("weather-status");
+let todayStats = document.getElementById("today-status");
+let todayMin = document.getElementById("today-min");
+let todayMax = document.getElementById("today-max");
+
+
 
 fetch(
 	`https://api.openweathermap.org/data/2.5/weather?q=tehran&units=metric&appid=0a76258bd2fbafc7cf09dd8c480abe2b`
 )
 	.then((response) => response.json())
 	.then((response) => {
-		displayData(response)
+		displayData(response);
 	})
 	.catch((err) => alert("error"));
 
 function displayData(obj) {
 	city.innerText = `${obj.name}`;
-  temp.textContent = `${Math.trunc(obj.main.temp)}`
-  weatherCondition.textContent = `${obj.weather[0].main}`
+	temp.textContent = `${Math.round(obj.main.temp)}`;
+	weatherCondition.textContent = `${obj.weather[0].main}`;
+	todayStats.textContent = `${obj.weather[0].main}`;
+  todayMin.textContent = `${Math.round(obj.main.temp_min)}`
+  todayMax.textContent = `${Math.round(obj.main.temp_max)}`
 }
